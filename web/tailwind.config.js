42c5665f2b3402678c73345bb7a5019c72d72e66
Alan Santos <alansods@gmail.com>
2025-05-05 01:25:26 -0300

    instalacao e configuracao tailwind css

---
+++ web/tailwind.config.js
@@ -0,0 +1,17 @@
+import { theme } from 'tailwindcss/defaultConfig'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Open Sans', ...theme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
